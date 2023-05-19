a0b7096200720cb2980efacb44092bc279b5bb89
MateKun200 <minor.kun@gmail.com>
2023-05-19 14:39:54 +0200

    some update on links

--- src/consts.ts
+++ src/consts.ts
@@ -37,10 +37,10 @@ export type Sidebar = Record<
 export const SIDEBAR: Sidebar = {
   en: {
     "Section Header": [
-      { text: "Introduction", link: "docs/en/introduction" },
-      { text: "Page 2", link: "docs/en/page-2" },
-      { text: "Page 3", link: "docs/en/page-3" },
+      { text: "Introduction", link: "/docs/en/introduction" },
+      { text: "Page 2", link: "/docs/en/page-2" },
+      { text: "Page 3", link: "/docs/en/page-3" },
     ],
-    "Another Section": [{ text: "Page 4", link: "docs/en/page-4" }],
+    "Another Section": [{ text: "Page 4", link: "/docs/en/page-4" }],
   },
 };
